435fe5b9a1acf9f9adf026e1da0960d73dbf8066
Korbinian Butz (kobu) <Korbinian.Butz@softwareag.com>
2022-07-22 21:05:26 +0200

    add third sample for piped input

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from subprocess import PIPE, Popen
 import subprocess
 import os
 
@@ -10,14 +11,14 @@ os.environ["C8Y_PASSWORD"] = "secret-password"
 os.environ["C8Y_SETTINGS_CI"] = "TRUE"
 
 
-# A simple GET command
+# OPTION 1: A simple GET command using subprocess.run()
 output = subprocess.run(["c8y", "devices", "list"], capture_output=True)
 print('return code: ', output.returncode)
 print('stdout: ', output.stderr.decode("utf-8") )
 print('stderr: ', output.stdout.decode('utf-8'))
 
 
-# Chain a command with static text (echo 'device_a\ndevice_b' | c8y devices create --dry')
+# OPTION 2: Chain a command with static text using subprocess.Popen (e.g. echo 'device_a\ndevice_b' | c8y devices create --dry')
 cmd = 'c8y devices create --dry'
 cmd_call = subprocess.Popen(cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 output, errors = cmd_call.communicate(input="device_a\ndevice_b\n")
@@ -26,3 +27,12 @@ print('return code: ', cmd_call.returncode)
 print('stdout: ', output)
 print('stderr: ', errors)
 
+
+# OPTION 3: Run any chained command using Popen
+cmd = 'seq -f "device_%05g" 1 2 | c8y devices create --dry'
+with Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True) as process:
+    output, error = process.communicate()
+    print('return code: ', cmd_call.returncode)
+    print('stdout: ', '' if output == '' else output.decode('utf-8'))
+    print('stderr: ', '' if error == '' else error.decode('utf-8') )
+    
