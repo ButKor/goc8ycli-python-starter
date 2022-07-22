a320dedd7fa17bd9b230f32e8f975244e836bd09
Korbinian Butz (kobu) <Korbinian.Butz@softwareag.com>
2022-07-22 21:14:08 +0200

    adding third option for piped input

--- app.py
+++ app.py
@@ -29,10 +29,10 @@ print('stderr: ', errors)
 
 
 # OPTION 3: Run any chained command using Popen
-cmd = 'seq -f "device_%05g" 1 2 | c8y devices create --dry'
+cmd = 'seq -f "device_%05g" 1 2 | c8y devicess create --dry'
 with Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True) as process:
     output, error = process.communicate()
-    print('return code: ', cmd_call.returncode)
-    print('stdout: ', '' if output == '' else output.decode('utf-8'))
-    print('stderr: ', '' if error == '' else error.decode('utf-8') )
+    print('return code: ', process.returncode)
+    print('stdout: ', 'none' if output == '' else output.decode('utf-8'))
+    print('stderr: ', 'none' if error == '' else error.decode('utf-8') )
     
